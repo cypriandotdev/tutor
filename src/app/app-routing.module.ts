ef0a2f9d6e894eb6db944a9d7117559b38f4a315
MercurialMune <munenecyp@gmail.com>
2021-10-22 16:08:18 +0300

    finalize tutorsite landing page

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,21 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import {PortfolioComponent} from "./portfolio/portfolio.component";
+import {ArticlesComponent} from "./articles/articles.component";
+import {TutorComponent} from "./tutor/tutor.component";
+import {CommonModule} from "@angular/common";
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:"portfolio",component:PortfolioComponent},
+  {path:"articles",component:ArticlesComponent},
+  {path:"",component:TutorComponent}
+];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
+  imports: [
+    CommonModule,
+    RouterModule.forRoot(routes)
+  ],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,25 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { TutorComponent } from './tutor/tutor.component';
+import { ArticlesComponent } from './articles/articles.component';
+import { PortfolioComponent } from './portfolio/portfolio.component';
+import {FormsModule} from "@angular/forms";
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    TutorComponent,
+    ArticlesComponent,
+    PortfolioComponent
   ],
   imports: [
+    FormsModule,
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
